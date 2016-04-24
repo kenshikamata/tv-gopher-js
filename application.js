080f1533cac7e8503e8fc2c86b0f53b6fed42445
kamata-ke <kamata-ke@klab.com>
2016-04-24 15:30:52 +0900

    test

---
+++ application.js
@@ -0,0 +1,81 @@
+//# sourceURL=application.js
+
+/*
+   application.js
+   sampletvml
+
+   Copyright (c) 2016年 kamata-ke. All rights reserved.
+   */
+
+/*
+ * This file provides an example skeletal stub for the server-side implementation 
+ * of a TVML application.
+ *
+ * A javascript file such as this should be provided at the tvBootURL that is 
+ * configured in the AppDelegate of the TVML application. Note that  the various 
+ * javascript functions here are referenced by name in the AppDelegate. This skeletal 
+ * implementation shows the basic entry points that you will want to handle 
+ * application lifecycle events.
+ */
+
+/**
+ * @description The onLaunch callback is invoked after the application JavaScript 
+ * has been parsed into a JavaScript context. The handler is passed an object 
+ * that contains options passed in for launch. These options are defined in the
+ * swift or objective-c client code. Options can be used to communicate to
+ * your JavaScript code that data and as well as state information, like if the 
+ * the app is being launched in the background.
+ *
+ * The location attribute is automatically added to the object and represents 
+ * the URL that was used to retrieve the application JavaScript.
+ */
+App.onLaunch = function(options) {
+    var javascriptFiles = [
+        `${options.BASEURL}main.js`,
+    ];
+
+    evaluateScripts(javascriptFiles, function(success) {
+        if (success) {
+            var alert = createAlert("hoge", "huga");
+            navigationDocument.pushDocument(alert);
+        } else {
+            var alert = createAlert("Evaluate Scripts Error", "There was an error attempting to evaluate the external JavaScript files.\n\n Please check your network connection and try again later.");
+            navigationDocument.presentModal(alert);
+
+            throw ("Playback Example: unable to evaluate scripts.");
+        }
+    });
+}
+
+
+App.onWillResignActive = function() {
+
+}
+
+App.onDidEnterBackground = function() {
+
+}
+
+App.onWillEnterForeground = function() {
+
+}
+
+App.onDidBecomeActive = function() {
+
+}
+
+App.onWillTerminate = function() {
+
+}
+
+
+/**
+ * This convenience funnction returns an alert template, which can be used to present errors to the user.
+ */
+var createAlert = function(title, description) {
+    var alertString = hoge();
+    var parser = new DOMParser();
+    var alertDoc = parser.parseFromString(alertString, "application/xml");
+    return alertDoc
+}
+
